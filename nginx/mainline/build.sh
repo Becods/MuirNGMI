8da83593d1098bb6bbd5deeea7e927119de93339
MuirNGMI <coder@muir.fun>
2022-01-26 23:10:07 +0800

    Add Action Files

--- nginx/mainline/build.sh
+++ nginx/mainline/build.sh
@@ -8,12 +8,12 @@ print(){
 
 print [Info]正在准备Nginx环境
 docker build . -t nginx:builder -f Dockerfile.builder
-print [Info]正在打包nginx
-docker build . -t nginx -f Dockerfile
 print [Info]正在准备Nginx模块环境
 docker build . -t nginx:modules -f Dockerfile.modules
 print [Info]正在打包nginx:full
 docker build . -t nginx:full -f Dockerfile.full
+print [Info]正在打包nginx
+docker build . -t nginx -f Dockerfile
 print [Info]正在制作nginx:fixed
 docker build . -t nginx:fixed -f Dockerfile.fixed
 print [Info]nginx制作完成
